5a229c3627d8abdf5c8a7c645bd95ffccb132cb4
Ai Bui <ai.bui@enablestartup.com>
2018-06-13 14:43:08 +0700

    send mail whitelist

--- routes/index.js
+++ routes/index.js
@@ -56,17 +56,25 @@ router.post('/add_whitelist', function(req, res, next) {
       email: emailToAdd,
     },
     defaults: {
+      email: emailToAdd,
       created_at: new Date(),
       updated_at: new Date(),
     }
   }).spread(
     (data, created) => {
       res.cookie('is_added_newsletter', 'true');
-  
+      
       if(!created){
         return failResponse(res, 'Email is added');
       }
+      
+      const msg = {
+        to: emailToAdd,
+        from: appConfig.email_sender_address,
+        templateId: "691924c8-b40b-4867-afdf-3e4241d150c6"
+      };
 
+      sgMail.send(msg);
       return successResponse(res, data);
     }
   ).catch((message) => {
